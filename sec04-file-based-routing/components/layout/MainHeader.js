e6c57e9bc963a9c2926ee0c99e7486ba999418fe
Satoshi T <sulp91_fy@yahoo.co.jp>
2022-04-21 08:20:06 +0900

    共通ヘッダ追加

---
+++ sec04-file-based-routing/components/layout/MainHeader.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import classes from "./MainHeader.module.css";
+export const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.log}>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">イベント一覧</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
